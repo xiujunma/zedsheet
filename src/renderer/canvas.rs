1bdd422b2d7486eacab313b725ae07e53c6a27e0
Xiujun Ma <xma@compusoftdevelopment.com>
2023-06-09 09:46:47 -0600

    wip

--- src/renderer/canvas.rs
+++ src/renderer/canvas.rs
@@ -8,6 +8,7 @@ use web_sys::ImageData;
 use web_sys::ImageBitmap;
 use web_sys::DomMatrix;
 use web_sys::CanvasWindingRule;
+use js_sys::Float64Array;
 
 pub enum LineCap {
     Butt,
@@ -128,8 +129,108 @@ impl Canvas {
         println!("draw");
     }
 
-    pub fn round_rect(&self, x: f64, y: f64, width: f64, height: f64, radius: f64) {
-        //TODO
+    // pub fn get_line_dash(&self) -> Vec<f64> {
+    //     let segments = self.ctx.get_line_dash();
+    //     let mut result = Vec::new();
+    //     for i in 0..segments.length() {
+    //         result.push(segments.get_index(i));
+    //     }
+    //     return result
+    // }
+
+    pub fn set_line_dash(&self, segments: &Vec<f64>) -> &Self {
+        // FIXME
+        let segments_js_value = Float64Array::new_with_length(segments.len() as u32);
+        for (i, segment) in segments.iter().enumerate() {
+            segments_js_value.set_index(i as u32, *segment);
+        }
+        self.ctx.set_line_dash(&segments_js_value).unwrap();
+        return self
+    }
+
+    pub fn craete_linear_gradient(&self, x0: f64, y0: f64, x1: f64, y1: f64) -> &Self {
+        self.ctx.create_linear_gradient(x0, y0, x1, y1);
+        return self
+    }
+
+    pub fn create_radial_gradient(&self, x0: f64, y0: f64, r0: f64, x1: f64, y1: f64, r1: f64) -> &Self {
+        self.ctx.create_radial_gradient(x0, y0, r0, x1, y1, r1).unwrap();
+        return self
+    }
+
+    pub fn create_pattern(&self, image: &ImageBitmap, repetition: &str) -> &Self {
+        self.ctx.create_pattern_with_image_bitmap(image, repetition).unwrap();
+        return self
+    }
+
+    pub fn bezier_curve_to(&self, cp1x: f64, cp1y: f64, cp2x: f64, cp2y: f64, x: f64, y: f64) -> &Self {
+        self.ctx.bezier_curve_to(cp1x, cp1y, cp2x, cp2y, x, y);
+        return self
+    }
+
+    pub fn quadratic_curve_to(&self, cpx: f64, cpy: f64, x: f64, y: f64) -> &Self {
+        self.ctx.quadratic_curve_to(cpx, cpy, x, y);
+        return self
+    }
+
+    pub fn arc(&self, x: f64, y: f64, radius: f64, start_angle: f64, end_angle: f64, counterclockwise: Option<bool>) -> &Self {
+        if counterclockwise.is_some() {
+            self.ctx.arc_with_anticlockwise(x, y, radius, start_angle, end_angle, counterclockwise.unwrap()).unwrap();
+        } else {
+            self.ctx.arc(x, y, radius, start_angle, end_angle).unwrap();
+        }
+        return self
+    }
+
+    pub fn arc_to(&self, x1: f64, y1: f64, x2: f64, y2: f64, radius: f64) -> &Self {
+        self.ctx.arc_to(x1, y1, x2, y2, radius).unwrap();
+        return self
+    }
+
+    pub fn begin_path(&self) -> &Self {
+        self.ctx.begin_path();
+        return self
+    }
+
+    pub fn close_path(&self) -> &Self {
+        self.ctx.close_path();
+        return self
+    }
+
+    pub fn move_to(&self, x: f64, y: f64) -> &Self {
+        self.ctx.move_to(x, y);
+        return self
+    }
+
+    pub fn line_to(&self, x: f64, y: f64) -> &Self {
+        self.ctx.line_to(x, y);
+        return self
+    }
+
+    pub fn ellipse(&self, x: f64, y: f64, radius_x: f64, radius_y: f64, rotation: f64, start_angle: f64, end_angle: f64, counterclockwise: Option<bool>) -> &Self {
+        if counterclockwise.is_some() {
+            self.ctx.ellipse_with_anticlockwise(x, y, radius_x, radius_y, rotation, start_angle, end_angle, counterclockwise.unwrap()).unwrap();
+        } else {
+            self.ctx.ellipse(x, y, radius_x, radius_y, rotation, start_angle, end_angle).unwrap();
+        }
+
+        return self
+    }
+
+    pub fn rect(&self, x: f64, y: f64, width: f64, height: f64) -> &Self {
+        self.ctx.rect(x, y, width, height);
+        return self
+    }
+
+    pub fn round_rect(&self, x: f64, y: f64, width: f64, height: f64, radius: f64) -> &Self {
+        self.begin_path()
+            .move_to(x + radius, y)
+            .arc_to(x + width, y, x + width, y + height, radius)
+            .arc_to(x + width, y + height, x, y + height, radius)
+            .arc_to(x, y + height, x, y, radius)
+            .arc_to(x, y, x + width, y, radius)
+            .close_path();
+        return self
     }
 
     pub fn fill(&self, rule: Option<CanvasWindingRule>) -> &Self {
@@ -191,8 +292,8 @@ impl Canvas {
         return self
     }
 
-    pub fn draw_image(&self, image: ImageBitmap, dx: f64, dy: f64) -> &Self {
-        self.ctx.draw_image_with_image_bitmap(&image, dx, dy).unwrap();
+    pub fn draw_image(&self, image: &ImageBitmap, dx: f64, dy: f64) -> &Self {
+        self.ctx.draw_image_with_image_bitmap(image, dx, dy).unwrap();
         return self
     }
 
