0cbc19a6e6abf114b10ffcee2ba65fe1055c2714
Xiujun Ma <xma@compusoftdevelopment.com>
2024-01-03 10:19:11 -0700

    update

--- src/core/cell_range.rs
+++ src/core/cell_range.rs
@@ -1,3 +1,6 @@
+use std::fmt::Display;
+use crate::renderer::alphabets::{ exp2xy, xy2expr };
+
 #[derive(Debug, Clone)]
 pub struct CellRange {
     pub sri: usize,
@@ -6,6 +9,19 @@ pub struct CellRange {
     pub eci: usize,
     pub w: f64,
     pub h: f64,
+}
+
+impl Display for CellRange {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+
+        let mut cell_ref = xy2expr(self.sri, self.sci);
+
+        if self.multiple() {
+            cell_ref = format!("{}:{}", cell_ref, xy2expr(self.eri, self.eci));
+        }
+
+        write!(f, "{}", cell_ref)
+    }
 }
 
 impl CellRange {
@@ -18,5 +34,37 @@ impl CellRange {
             w: 0f64,
             h: 0f64,
         }
+    }
+
+    pub fn set(&mut self, sri: usize, sci: usize, eri: usize, eci: usize) {
+        self.sri = sri;
+        self.sci = sci;
+        self.eri = eri;
+        self.eci = eci;
+    }
+
+    pub fn multiple(&self) -> bool {
+        self.sri != self.eri || self.sci != self.eci
+    }
+
+    pub fn includes(&self, cell_ref: &str) -> bool {
+        let (ri, ci) = exp2xy(cell_ref);
+        return self.sri < ri && self.eri > ri && self.sci < ci && self.eci > ci;
+    }
+
+    pub fn each(&self, f: impl Fn(usize, usize)) {
+        for ri in self.sri..=self.eri {
+            for ci in self.sci..=self.eci {
+                f(ri, ci);
+            }
+        }
+    }
+
+    pub fn contains(&self, range: Self) -> bool {
+        self.sri <= range.sri && self.sci <= range.sci && self.eri >= range.eri && self.eci >= range.eci
+    }
+
+    pub fn within(&self, range: Self) -> bool {
+        self.sri >= range.sri && self.sci >= range.sci && self.eri <= range.eri && self.eci <= range.eci
     }
 }
---
+++ src/core/cols.rs
@@ -0,0 +1,7 @@
+pub struct Cols {
+    pub length: usize,
+    pub width: f64,
+    pub indexWidth: f64,
+    pub minWidth: f64,
+}
+
